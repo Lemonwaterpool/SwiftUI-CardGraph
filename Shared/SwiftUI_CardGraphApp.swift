4429cca35ed0d9df55ddb6c6950789392a27bfd4
Lemonwaterpool <frqk3624@gmail.com>
2020-10-28 19:03:24 +0900

    Initial Commit

---
+++ Shared/SwiftUI_CardGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CardGraphApp.swift
+//  Shared
+//
+//  Created by H Chan on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CardGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
